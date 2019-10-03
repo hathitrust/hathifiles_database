a6d45ffc5478b342f4a347ac3ceba59aacdf0eba
Bill Dueber <bill@dueber.com>
2019-10-03 11:07:06 -0400

    Flesh out the line code

---
+++ lib/hathifiles_database/db/connection.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+module HathifilesDatabase
+  class Connection
+    def initialize(connection_string)
+
+    end
+  end
+end
---
+++ lib/hathifiles_database/db/migrations/001_initial_setup.rb
@@ -0,0 +1,8 @@
+require 'sequel'
+Sequel.extension :migration
+
+Sequel.migration do
+  up do
+
+  end
+end
